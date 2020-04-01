9b50a1ae75e14a64e6cf1a9d47325e56e3baa0d0
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2020-04-01 10:24:30 -0300

    Project commit

---
+++ src/components/ImageList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-if="isLoggedIn" class="image-container">
+            <img v-for="image in allImages" :key="image.id" :src="image.link" />
+        </div>
+        <h2 v-else>Log in to get started!</h2>
+    </div>
+</template>
+
+<script>
+    import { mapActions, mapGetters } from 'vuex'
+
+    export default {
+        name: "ImageList",
+        methods: mapActions(['fetchImages']),
+        created() {
+            this.fetchImages()
+        },
+        computed: mapGetters(['allImages', 'isLoggedIn'])
+    }
+</script>
+
+<style scoped>
+    .image-container {
+        column-count: 3;
+        column-gap: 0;
+    }
+
+    img {
+        max-width: 100%;
+        padding: 5px;
+    }
+</style>
